8182ad19af36f7b56f79205977915591f9258951
MayokunYus <mayokunyusuf0@gmail.com>
2026-06-08 22:46:07 +0100

    my anime app

---
+++ clean-anime/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "AniWorld",
+  description: "Search anime",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
